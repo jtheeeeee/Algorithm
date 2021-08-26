8d9779c0c0deaab501c071968393df3a7ac9e750
jtheeeeee <j_03_17@naver.com>
2021-08-26 22:12:28 +0900

    0826 algorithm

--- java/string/src/com/company/ioc/string0826/Main.java
+++ java/string/src/com/company/ioc/string0826/Main.java
@@ -1,7 +1,29 @@
 package com.company.ioc.string0826;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+       /*  backjoon 10808
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int[] arr = new int[26];
+        for (int i = 0 ; i<s.length(); i++){
+            arr[s.charAt(i)-97]++;
+        }
+        for (int index : arr){
+            System.out.print(index+" ");
+        }
+        */
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int count = 0;
+        char [] arr = {'c','d','l','n','s','z'};
+        for (int i=0; i<s.length(); i++){
+
+        }
+        System.out.println(count);
+
+
     }
 }
---
+++ python/string/0826/string.py
@@ -0,0 +1,27 @@
+# s= input()
+# char_list =['c-','c=','d-','dz','lj','nj','s=','z=']
+# count = 0
+# i=0
+# except_list=['-','=','q','w','x']
+# while i <len(s):
+#     if s[i:i+2] in char_list:
+#         count+=1
+#         i+=1
+#         if s[i-1:i+1]=='dz' and s[i+1:i+2] != '=' :
+#             count -=1
+#     else:
+#         if s[i:i+1] in except_list:
+#             i+=1
+#             continue
+#         count+=1
+#     i+=1
+# print(count)
+
+#특정 문자열을 list로 만들어야 겠다는 접근법 까지는 꽤 좋았으나
+#특정 문자열을 하나의 단어로 대체하겠다는 접근은 하지 못함..
+s=input()
+char_list =['c-','c=','d-','dz=','lj','nj','s=','z=']
+for i in char_list:
+    s = s.replace(i, "*")
+
+print(len(s))
