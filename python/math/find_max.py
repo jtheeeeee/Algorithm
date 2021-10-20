052c2e4b730cc00c2bcbc47afa24d5629498c410
jtheeeeee <j_03_17@naver.com>
2021-10-20 19:39:34 +0900

    math

---
+++ python/math/find_max.py
@@ -0,0 +1,17 @@
+input = [3, 5, 6, 1, 2, 4]
+
+
+def find_max_num(array):
+    # 이 부분을 채워보세요!
+    for num in array:
+        for compare_num in array:
+            if num < compare_num:
+                print(1,num, compare_num)
+                break
+        else:
+            print(2, num, compare_num)
+            return num
+
+
+result = find_max_num(input)
+print(result)
